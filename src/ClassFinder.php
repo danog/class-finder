d5fb2dcee6bb2f8a96634fda5913c78076c90e8e
Hayden Pierce <hayden@haydenpierce.com>
2018-07-14 08:27:13 -0500

    Initial commit - copy content from Stack Overflow

---
+++ src/ClassFinder.php
@@ -0,0 +1,51 @@
+<?php
+namespace HaydenPierce\ClassFinder;
+
+class ClassFinder
+{
+    //This value should be the directory that contains composer.json
+    const appRoot = __DIR__ . "/../../";
+
+    public static function getClassesInNamespace($namespace)
+    {
+        $files = scandir(self::getNamespaceDirectory($namespace));
+
+        $classes = array_map(function($file) use ($namespace){
+            return $namespace . '\\' . str_replace('.php', '', $file);
+        }, $files);
+
+        return array_filter($classes, function($possibleClass){
+            return class_exists($possibleClass);
+        });
+    }
+
+    private static function getDefinedNamespaces()
+    {
+        $composerJsonPath = self::appRoot . 'composer.json';
+        $composerConfig = json_decode(file_get_contents($composerJsonPath));
+
+        //Apparently PHP doesn't like hyphens, so we use variable variables instead.
+        $psr4 = "psr-4";
+        return (array) $composerConfig->autoload->$psr4;
+    }
+
+    private static function getNamespaceDirectory($namespace)
+    {
+        $composerNamespaces = self::getDefinedNamespaces();
+
+        $namespaceFragments = explode('\\', $namespace);
+        $undefinedNamespaceFragments = [];
+
+        while($namespaceFragments) {
+            $possibleNamespace = implode('\\', $namespaceFragments) . '\\';
+
+            if(array_key_exists($possibleNamespace, $composerNamespaces)){
+                return realpath(self::appRoot . $composerNamespaces[$possibleNamespace] . implode('/', $undefinedNamespaceFragments));
+            }
+
+            $undefinedNamespaceFragments[] = array_pop($namespaceFragments);
+        }
+
+        return false;
+    }
+}
