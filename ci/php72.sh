b5ef80c01febc483d7e614502f8aed618d6b3eec
Hayden Pierce <hayden@haydenpierce.com>
2018-09-03 11:17:17 -0500

    Correct paths

--- ci/php72.sh
+++ ci/php72.sh
@@ -1,2 +1,2 @@
-composer install --working-dir=/build/hpierce1102/ClassFinder
+composer install --working-dir=/builds/hpierce1102/ClassFinder/test/app1
 php /builds/hpierce1102/ClassFinder/vendor/bin/phpunit /builds/hpierce1102/ClassFinder/test/app1/src/ClassFinderTest.php
