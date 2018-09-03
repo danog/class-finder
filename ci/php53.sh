0c3b4c1ac1c89a13d8b3079f73cf708f1833e344
Hayden Pierce <hayden@haydenpierce.com>
2018-09-03 16:25:03 -0500

    Add PHP 5.3 tests

--- ci/php53.sh
+++ ci/php53.sh
@@ -1,1 +1,3 @@
-echo "completed.";
+composer install --working-dir=/builds/hpierce1102/ClassFinder/test/app1 --quiet
+composer install --working-dir=/builds/hpierce1102/ClassFinder --quiet
+php /builds/hpierce1102/ClassFinder/vendor/bin/phpunit /builds/hpierce1102/ClassFinder/test/app1/src/ClassFinderTest.php
