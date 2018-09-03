4b19db708ff9ee7d2d755f58fd64e2fa4b70f1fa
Hayden Pierce <hayden@haydenpierce.com>
2018-09-03 16:53:13 -0500

    Add PHP version to CI output

--- ci/php53.sh
+++ ci/php53.sh
@@ -1,3 +1,4 @@
 composer install --working-dir=/builds/hpierce1102/ClassFinder/test/app1 --quiet
 composer install --working-dir=/builds/hpierce1102/ClassFinder --quiet
+php --version
 php /builds/hpierce1102/ClassFinder/vendor/bin/phpunit /builds/hpierce1102/ClassFinder/test/app1/src/ClassFinderTest.php
