3df580a4447cd30378ffc2cd2b4f2eb2d8fd7c4b
Hayden Pierce <hayden@haydenpierce.com>
2018-09-03 11:08:37 -0500

    Add new PHP 7.2 stuff (since the PHP5.3 stuff will never work due to PHPUnit being too new)

---
+++ ci/php72.sh
@@ -0,0 +1,2 @@
+composer install --working-dir=/build/hpierce1102/ClassFinder
+php /builds/hpierce1102/ClassFinder/vendor/bin/phpunit /builds/hpierce1102/ClassFinder/test/app1/src/ClassFinderTest.php
