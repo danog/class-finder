77aa8b946064a89cbd4e621855d17d0cd009093c
Hayden Pierce <hayden@haydenpierce.com>
2018-09-29 13:27:19 -0500

    Add some minimal test harness.

---
+++ test/app1/files/RedHerring.php
@@ -0,0 +1,3 @@
+<?php
+
+namespace TestApp1\FilesClasses;
---
+++ test/app1/src/FilesTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace TestApp1;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use HaydenPierce\ClassFinder\ClassFinder;
+
+class FilesTest extends \PHPUnit_Framework_TestCase
+{
+    public function setup()
+    {
+        // Reset ClassFinder back to normal.
+        ClassFinder::setAppRoot(null);
+    }
+    /**
+     * @dataProvider classFinderDataProvider
+     */
+    public function testClassFinder($namespace, $expected, $message)
+    {
+        try {
+            $classes = ClassFinder::getClassesInNamespace($namespace);
+        } catch (\Exception $e) {
+            $this->assertFalse(true, 'An exception occurred: ' . $e->getMessage());
+            $classes = array();
+        }
+
+        $this->assertEquals($expected, $classes, $message);
+    }
+
+    public function classFinderDataProvider()
+    {
+        return array(
+            array(
+                'TestApp1\FilesClasses',
+                array(
+                    'TestApp1\FilesClasses\Bam',
+                    'TestApp1\FilesClasses\Wam',
+                    'TestApp1\FilesClasses\Fam',
+                    'TestApp1\FilesClasses\Cam',
+                    'TestApp1\FilesClasses\Lam',
+                ),
+                'ClassFinder should be able to find 1st party classes included from `files` listed in composer.json.'
+            )
+        );
+    }
+}
