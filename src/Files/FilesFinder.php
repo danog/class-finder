631e09d80224ecdd60e2cc96439dfda6de2dbc4e
Hayden Pierce <hayden@haydenpierce.com>
2018-10-21 09:39:21 -0500

    Provide a more helpful error message if files support fails due to exec() being disabled.

--- src/Files/FilesFinder.php
+++ src/Files/FilesFinder.php
@@ -1,15 +1,28 @@
 <?php
 namespace HaydenPierce\ClassFinder\Files;
 
+use HaydenPierce\ClassFinder\Exception\ClassFinderException;
 use HaydenPierce\ClassFinder\FinderInterface;
 
 class FilesFinder implements FinderInterface
 {
     private $factory;
 
+    /**
+     * FilesFinder constructor.
+     * @param FilesEntryFactory $factory
+     * @throws ClassFinderException
+     */
     public function __construct(FilesEntryFactory $factory)
     {
         $this->factory = $factory;
+
+        if (!function_exists('exec')) {
+            throw new ClassFinderException(sprintf(
+                'FilesFinder requires that exec() is available. Check your php.ini to see if it is disabled. See "%s" for details.',
+                'https://gitlab.com/hpierce1102/ClassFinder/blob/master/docs/exceptions/filesExecNotAvailable.md'
+            ));
+        }
     }
 
     public function isNamespaceKnown($namespace)
