b5a075ffd68f5a658186b854ae7c893d7b127650
Hayden Pierce <hayden@haydenpierce.com>
2018-12-31 12:54:44 -0600

    Add failing unit test for classmap recursive mode

--- test/app1/src/ClassmapTest.php
+++ test/app1/src/ClassmapTest.php
@@ -68,4 +68,43 @@ class ClassmapTest extends \PHPUnit_Framework_TestCase
             )
         );
     }
+
+    /**
+     * @dataProvider classesInNamespaceRecursivelyDataProvider
+     */
+    public function testClassesInNamespaceRecursivelyDataProvider($namespace, $expected, $message)
+    {
+        try {
+            $classes = ClassFinder::getClassesInNamespace($namespace, ClassFinder::RECURSIVE_MODE);
+        } catch (Exception $e) {
+            $this->assertFalse(true, 'An exception occurred: ' . $e->getMessage());
+            $classes = array();
+        }
+
+        $this->assertEquals($expected, $classes, $message);
+    }
+
+    public function classesInNamespaceRecursivelyDataProvider()
+    {
+        return array(
+            array(
+                'TestApp1\ClassmapClasses',
+                array(
+                    'TestApp1\ClassmapClasses\Bik',
+                    'TestApp1\ClassmapClasses\Bil',
+                    'TestApp1\ClassmapClasses\Bir',
+                    'TestApp1\ClassmapClasses\Mik',
+                    'TestApp1\ClassmapClasses\Mil',
+                    'TestApp1\ClassmapClasses\Mir',
+                    'TestApp1\ClassmapClasses\Tik',
+                    'TestApp1\ClassmapClasses\Til',
+                    'TestApp1\ClassmapClasses\Tir',
+                    'TestApp1\ClassmapClasses\NestedClasses\NestedClass1',
+                    'TestApp1\ClassmapClasses\NestedClasses\NestedClass2',
+                    'TestApp1\ClassmapClasses\NestedClasses\NestedClass3',
+                ),
+                'Classfinder should be able to load classes based on a classmap.'
+            )
+        );
+    }
 }
