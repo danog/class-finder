2bafb5a20cf3d7ac2fe13c5580bc10b4e9402756
Hayden Pierce <hayden@haydenpierce.com>
2019-02-03 08:50:13 -0600

    Cause a failing build hopefully

--- ci/php53.sh
+++ ci/php53.sh
@@ -1,4 +1,4 @@
-composer install --working-dir=$1/test/app1 --quiet
-composer install --working-dir=$1 --quiet
+composer install --working-dir=$1/test/app1 --quiet || exit 1
+composer install --working-dir=$1 --quiet || exit 1
 php --version
 php /builds/hpierce1102/ClassFinder/vendor/bin/phpunit --testsuite all
