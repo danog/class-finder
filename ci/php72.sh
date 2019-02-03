333817fc022b1176d4226af2d4504b1116ae2f6e
Hayden Pierce <hayden@haydenpierce.com>
2019-02-03 09:55:04 -0600

    Remove another hardcoded hpierce1102

--- ci/php72.sh
+++ ci/php72.sh
@@ -1,6 +1,4 @@
-echo "composer install --working-dir=$1/test/app1 --quiet"
-
-composer install --working-dir=$1/test/app1 --quiet
-composer install --working-dir=$1 --quiet
+composer install --working-dir=$1/test/app1 --quiet || exit 1
+composer install --working-dir=$1 --quiet || exit 1
 php --version
-php /builds/hpierce1102/ClassFinder/vendor/bin/phpunit --testsuite all
+php $1/vendor/bin/phpunit --testsuite all
