bcee6be6b5b23ffd7247b7f5f49601a02c742808
Hayden Pierce <hayden@haydenpierce.com>
2019-02-03 08:41:40 -0600

    Test CI for forked repos

--- ci/php72.sh
+++ ci/php72.sh
@@ -1,4 +1,4 @@
-composer install --working-dir=/builds/hpierce1102/ClassFinder/test/app1 --quiet
-composer install --working-dir=/builds/hpierce1102/ClassFinder --quiet
+composer install --working-dir=$1/test/app1 --quiet
+composer install --working-dir=$1 --quiet
 php --version
 php /builds/hpierce1102/ClassFinder/vendor/bin/phpunit --testsuite all
