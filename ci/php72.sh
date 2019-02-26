f3d7e879387593c5c697b257da724706a219a54f
Hayden Pierce <hayden@haydenpierce.com>
2019-02-25 18:41:26 -0600

    CI Testing

--- ci/php72.sh
+++ ci/php72.sh
@@ -3,4 +3,3 @@ composer install --working-dir=$1/test/app2 --quiet || exit 1
 composer install --working-dir=$1 --quiet || exit 1
 php --version
 php $1/vendor/bin/phpunit --testsuite all
-php $1/vendor/bin/phpunit --testsuite noAutoload
